55a0be52fa1872ef4fa42328f917effa3e3e8edd
capernicon <mattjablott@gmail.com>
2021-03-08 22:43:38 +0000

    Fix include of header file at index

---
+++ app/views/home/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>
+      APISim
+    </title>
+    <link rel="stylesheet" href="css/materialize.css"/>
+    <link rel="stylesheet" href="css/media.css"/>
+    <link rel="stylesheet" href="css/custom.css"/>
+
+    <script src="js/materialize.js"></script>
+    <script src="js/scripts.js"></script>
+  </head>
+
+  <body>
